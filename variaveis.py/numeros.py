188d1392bd450d5e9642fe4281012d184254cb74
marcos gomes <marcos.gomes@modenaesilva.com.br>
2024-09-17 17:16:01 -0400

    save

---
+++ variaveis.py/numeros.py
@@ -0,0 +1,5 @@
+inteiro = 3
+print('Inteiro = ', inteiro)
+
+real = 3.14
+print('Inteiro = ', real)
---
+++ variaveis.py/texto.py
@@ -0,0 +1,13 @@
+nome_completo = "Marcos Vini"
+
+nome_completo_aspas = """Marcos 
+Vini"""
+
+nome_completo_quebra = "Marcos \ Vini"
+
+print("Nome completo:", nome_completo.upper())
+print("Nome completo:" + nome_completo.lower())
+
+
+
+
