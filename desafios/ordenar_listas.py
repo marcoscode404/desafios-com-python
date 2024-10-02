fddb014ad348297201d9d4838fb832696d24780a
marcos gomes <marcos.gomes@modenaesilva.com.br>
2024-10-02 11:56:50 -0400

    save

---
+++ desafios/ordenar_listas.py
@@ -0,0 +1,10 @@
+list = [124, 10, 11,12,13,15,20,200,300,500,0,5,7]
+list.sort() # altera a lista que já existe
+# list.sort(reverse=True)
+# list.reverse()
+
+
+list2 = sorted(list) # altera e retorna
+
+print(list2)
+print(list)
