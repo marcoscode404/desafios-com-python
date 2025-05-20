995ea1bcc4bb81f75e7b1ccbac4da20e1d147b4f
Marcos Gomes <marcos.gomes@modenaesilva.com.br>
2025-05-20 14:26:37 -0400

    feat: add funcionalidade de atualizar e ver tarefas

--- aulas_rocketseat/gerenciador.py
+++ aulas_rocketseat/gerenciador.py
@@ -4,6 +4,22 @@ def adicionar_tarefa(tarefas, nome_tarefa="tarefa padrão"):
     print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
     return
 
+def ver_tarefas(tarefas):
+    print("\n Lista de tarefas:")
+    for indice, tarefa in enumerate(tarefas, start=1):
+        status = "✅" if tarefa["completada"] else " "
+        nome_tarefa = tarefa["tarefa"]
+        print(f"{indice}. [{status}] {nome_tarefa}") 
+
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+        tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
+        print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+    else:
+        print("Índice de tarefa inválido")
+    return
+
 tarefas = []
 while True:
     print("\n Menu do gerenciador de lista de tarefas")
@@ -19,6 +35,14 @@ while True:
     if escolha == "1":
         nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
         adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "2":
+        ver_tarefas(tarefas)
+    elif escolha == "3":
+        ver_tarefas(tarefas)
+        indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
+        novo_nome = input("Digite o novo nome da tarefa: ")
+        atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
+
     elif escolha == '6':
         break  
     print("Programa finalizado")
