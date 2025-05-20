132f460e9aa2b580d6549a6e12222b96b656624b
Marcos Gomes <marcos.gomes@modenaesilva.com.br>
2025-05-20 13:01:57 -0400

    save

---
+++ aulas_rocketseat/gerenciador.py
@@ -0,0 +1,14 @@
+while True:
+    print("\n Menu do gerenciador de lista de tarefas")
+    print("1. Adicionar tarefa")
+    print("2. Ver tarefas")
+    print("3. Atualizar Tarefa")
+    print("4. Completar tarefa")
+    print("5. Deletar tarefa")
+    print("6. Sair")
+
+    escolha = input("Digite a sua tarefa: ")
+
+    if escolha == '6':
+        break  
+    print("Programa finalizado")
